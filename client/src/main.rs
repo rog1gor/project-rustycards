5a2a7770a3b88f6fa9d41e220f58c9f91ac1ce96
obukaj <owczarek.jakub@gmail.com>
2022-12-23 12:34:19 +0100

    Fixing main

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,18 +1,5 @@
-<<<<<<< HEAD
 mod card;
 use card::Minion;
-fn main() {
-    let mut minion1 = Minion::new("rog1gor".to_string(), 2, 5);
-    let mut minion2 = Minion::new("obukaj".to_string(), 1, 3);
-    println!("{}", minion1.to_string());
-    println!("{}", minion2.to_string());
-    minion1.attack(&mut minion2);
-    println!("{}", minion1.to_string());
-    println!("{}", minion2.to_string());
-    minion1.attack(&mut minion2);
-    println!("{}", minion1.to_string());
-    println!("{}", minion2.to_string());
-=======
 mod action;
 mod p2p_client;
 
@@ -66,5 +53,4 @@ fn main() {
     Client::send_action(opponent_stream);
 
     action_handler.join();
->>>>>>> 8e101efdd07eeb87edd19576458496874ad5d538
 }
